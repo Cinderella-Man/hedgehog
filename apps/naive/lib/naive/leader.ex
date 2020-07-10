adc156fb00d1d267a5d0afde217ed284364ac140
Kamil Skowron <emanuel1989@gmail.com>
2020-07-10 13:08:39 +0100

    Leader and symbol supervisor added

---
+++ apps/naive/lib/naive/leader.ex
@@ -0,0 +1,15 @@
+defmodule Naive.Leader do
+  use GenServer
+
+  def start_link(symbol) do
+    GenServer.start_link(
+      __MODULE__,
+      symbol,
+      name: :"#{__MODULE__}-#{symbol}"
+    )
+  end
+
+  def init(symbol) do
+    {:ok, %{symbol: symbol}}
+  end
+end
