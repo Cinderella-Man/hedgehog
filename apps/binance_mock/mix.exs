b154afe83681661cfc62d1c5dcd2d93cb78d95ca
Kamil Skowron <emanuel1989@gmail.com>
2020-09-07 11:56:07 +0100

    BinanceMock mix exs updated

--- apps/binance_mock/mix.exs
+++ apps/binance_mock/mix.exs
@@ -26,6 +26,10 @@ defmodule BinanceMock.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:binance, "~> 0.7.1"},
+      {:decimal, "~> 1.0"},
+      {:phoenix_pubsub, "~> 2.0"},
+      {:streamer, in_umbrella: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
