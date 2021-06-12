11f8fd38d54506f813ddfc32a3b9d5ef26fc415e
Kamil Skowron <emanuel1989@gmail.com>
2021-06-12 22:08:27 +0100

    Binance module updated in the naive mix file, all deps autoupdated

--- apps/naive/mix.exs
+++ apps/naive/mix.exs
@@ -26,7 +26,7 @@ defmodule Naive.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:binance, "~> 0.7.1"},
+      {:binance, "~> 0.7"},
       {:core, in_umbrella: true},
       {:decimal, "~> 1.0"},
       {:ecto_sql, "~> 3.0"},
--- apps/streamer/mix.exs
+++ apps/streamer/mix.exs
@@ -26,7 +26,7 @@ defmodule Streamer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:binance, "~> 0.7.1"},
+      {:binance, "~> 0.7"},
       {:core, in_umbrella: true},
       {:ecto_sql, "~> 3.0"},
       {:ecto_enum, "~> 1.4"},
